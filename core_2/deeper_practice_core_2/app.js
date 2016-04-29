0bc1858e224e1f10e300d4a4f346fae0ce14b9c9
MKS-students <mks.students@hackreactor.com>
2016-04-29 16:08:08 -0700

    Completed Deeper Practice for modules

--- core_2/deeper_practice_core_2/app.js
+++ core_2/deeper_practice_core_2/app.js
@@ -1,7 +1,14 @@
 
-(function(){
+(function Server(){
 
-  function makeAjaxCall(){
+  function makeAjaxCall(object){
+    //To keep things simpler, let's just pretend that
+    //an actual Ajax call is made here. So instead,
+    //this function should only call displayMessage(..)
+    //with a generic success message.
+
+    registerUsers.displayMessage("You are a wonderful human being, worthy of love.");
+
 
 }
 
@@ -20,40 +27,104 @@
 
 (function registerUsers(){
 
+  function init(){
+    registerUsers.submitRegistration();
+  }
+
   function submitRegistration(){
   //this function pulls out values from the DOM elements
+
+  var userEmail, userFullName, userPhone;
+
+  $("input#submit").on("click", function(){
+    userEmail = $("#email").val();
+    console.log(userEmail);
+    $("#email").val('');
+    userFullName = $("#fullname").val();
+    $("#fullname").val('');
+    userPhone = $("#phone").val();
+    $("#phone").val('');
+
+  });
+
   //It should check to see if these values are valid
+
+  if(userEmail) {
+    var isValidEmail = registerUsers.processEmail(userEmail);}
+  if(userFullName){
+    var isValidName = registerUsers.processFullName(userFullName);}
+  if(userPhone){
+    var isValidPhone = registerUsers.processPhone(userPhone);
+  }
+
   //, and if so, add each of the values as properties
-  // (of the same name) to an object and pass that object
+  // (of the same name) to an object
+  //and pass that object
   // to a function called makeAjaxCall(..).
   //However, if any validation fails, an error message
   //should be passed to displayMessage(..).
+
+  var callObject = {};
+
+  if(isValidPhone && isValidName && isValidEmail){
+    callObject.email = userEmail;
+    callObject.fullname = userFullName;
+    callObject.phone = userPhone;
+
+    Server.makeAjaxCall(callObject);
+    console.log(callObject);
+
+  } else {
+    registerUsers.displayMessage("VALIDATION FAILED!!!");
+  }
+
+
+
 }
-  function processEmail(){
+  function processEmail(address){
     //must be between 5 and 100 characters in length,
     // and must contain exactly one "@" symbol
 
+    var pattern = /\w+@\w+.(com|net|org|edu){5,100}/gi;
+
+    if (address.match(pattern)){
+      return true
+    } else {
+      return false;
+    }
+
   }
 
-  function processFullName(){
+  function processFullName(name){
     // must be more than 3 characters
+    if (name.length < 3){
+      return false;
+    }
+    return true;
 
   }
 
-  function processPhone(){
+  function processPhone(number){
     //must be formatted as XXX-XXX-XXXX where X has to be a digit 0-9.
+    var pattern = /\w{3}-\w{3}-\w{4}/gi;
+
+    if (number.match(pattern)){
+      return true
+    } else {
+      return false;
+    }
 
   }
 
-  function displayMessage(){
+  function displayMessage(message){
     //sets its value into the #message DOM element
-
-  }
+    $("#message").append(message);
 
 
   }
 
   var publicAPI = {
+    init: init,
     submitRegistration: submitRegistration,
     processEmail: processEmail,
     processPhone: processPhone,
@@ -64,9 +135,12 @@
   return publicAPI;
 
 
-
+  }
 
 
   )();
+
+  $(document).ready(registerUsers.init());
+  console.log("ready");
 
 
